22165a92f7176a0d6683a625f0331641c1ebc6cf
eetusva <66327110+eetusva@users.noreply.github.com>
2024-10-09 12:11:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,50 @@
+const CACHE_NAME = 'ressukisa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/kilpailu.html',
+  '/kilpailijanluonti.html',
+  '/laskuri.html',
+  '/tulokset.html',
+  '/styles_etusivu.css',
+  '/kisanluonti.js',
+  '/ressulaskuri.jpg'
+];
+
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('ressulaskuri-cache').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/styles_etusivu.css',
-          '/ressulaskuri.jpg',
-          '/kilpailu.html',
-          '/kilpailijanluonti.html',
-          '/laskuri.html',
-          '/tulokset.html',
-          '/icon192.png',
-          '/icon512.png'
-        ]);
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
       })
-    );
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response; // Palauta välimuistista
+        }
+        return fetch(event.request); // Tee verkkopyyntö, jos ei ole välimuistissa
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName); // Poista vanhat välimuistit
+          }
+        })
+      );
+    })
+  );
+});
